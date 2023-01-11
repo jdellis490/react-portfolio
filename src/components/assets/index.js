9167bf53903813b2fc710c3b5e38f218ab120454
Justin Ellis <jdellis490@gmail.com>
2023-01-10 20:28:33 -0700

    Added sample projects and styling

---
+++ src/components/assets/index.js
@@ -0,0 +1,15 @@
+import passGen from './PasswordGenerator.png';
+import weatherDash from './Weather-Dashboard.png';
+import workayPlan from './WorkDay-Planner.png';
+import animeSearch from './Anime-Search-Engine.png';
+import jateEdit from './JATE-text-editor.png';
+import socialAPI from './Social-Network-API.png';
+
+export {
+    passGen,
+    weatherDash,
+    workayPlan,
+    animeSearch,
+    jateEdit,
+    socialAPI
+};
--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,19 +1,127 @@
-import React from 'react'
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import {
+  passGen,
+  weatherDash,
+  workayPlan,
+  animeSearch,
+  jateEdit,
+  socialAPI,
+} from "../assets/index";
 
 const Project = () => {
   return (
-    <div>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue nisl quam, eu sagittis velit scelerisque nec. In hac habitasse platea dictumst. Quisque sagittis massa vitae enim dapibus fermentum. Duis vitae tempor justo, eu tempus est. In imperdiet iaculis metus vel feugiat. Fusce sit amet orci sed massa pulvinar fermentum eget a lacus. Nunc auctor imperdiet consectetur. Proin varius dolor eu ante condimentum vehicula. Etiam finibus pulvinar faucibus. Curabitur nec eros lobortis, sodales odio sed, placerat erat. Morbi vitae dui risus. Maecenas efficitur eget elit in ornare. Duis a turpis elementum, lobortis massa vel, porta urna. Fusce fringilla ante eu nibh faucibus, in fermentum lacus accumsan. Integer congue sapien velit, quis rhoncus nisi fringilla at.
+    <div className="project-container">
+        <Card className="project">
+          <Card.Img variant="top" src={passGen} />
+          <Card.Body>
+            <Card.Title>Password Generator</Card.Title>
+            <Card.Text>
+              I built this project to create tool for users to generate complex
+              and secure passwords that they can use for various accounts.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small>
+              <Button variant="success"><a href="https://jdellis490.github.io/HW3-PasswordGen/">
+               </a>Deployment</Button>{' '}
+              <Button variant="secondary"><a href="https://github.com/jdellis490/HW3-PasswordGen"></a>GitHub</Button>
+            </small>
+          </Card.Footer>
+        </Card>
+        <Card className="project">
+          <Card.Img variant="top" src={weatherDash} />
+          <Card.Body>
+            <Card.Title>Weather Dashboard</Card.Title>
+            <Card.Text>
+              This project uses and API to get the weather conditions and
+              temperature of different cities across the country.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small>
+            <Button variant="success"><a href="https://jdellis490.github.io/Weather-Dashboard-HW6/">
+              </a>Deployment</Button>{' '}
+              <Button variant="secondary"><a href="https://github.com/jdellis490/Weather-Dashboard-HW6">
+                </a>GitHub</Button>
+            </small>
+          </Card.Footer>
+        </Card>
+          <Card className="">
+            <Card.Img variant="top" src={workayPlan} />
+            <Card.Body>
+              <Card.Title>Work Day Planner</Card.Title>
+              <Card.Text>
+                Project that allows user to input notes or reminders for
+                different parts of the day based on the hour. Has past, present,
+                and future color indicators as well.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small>
+              <Button variant="success"><a href="https://jdellis490.github.io/Day-Planner-HW5">
+                </a>Deployment</Button>{' '}
+                <Button variant="secondary"><a href="https://github.com/jdellis490/Day-Planner-HW5">
+                </a>GitHub</Button>
+              </small>
+            </Card.Footer>
+          </Card>
+          <Card className="project">
+            <Card.Img variant="top" src={animeSearch} />
+            <Card.Body>
+              <Card.Title>Anime Search Engine</Card.Title>
+              <Card.Text>
+                Search engine that uses multiple API calls to fetch anime
+                titles, descriptions, ratings, and YouTube trailers from anime
+                title input.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small>
+              <Button variant="success"><a href="https://skyw41k3r.github.io/project_1/ "></a>Deployment</Button>{' '}
+                <Button variant="secondary"><a href="https://github.com/Skyw41k3r/project_1"></a>GitHub</Button>
+              </small>
+            </Card.Footer>
+          </Card>
+        <Card className="">
+          <Card.Img variant="top" src={jateEdit} />
+          <Card.Body>
+            <Card.Title>JATE Text Editor</Card.Title>
+            <Card.Text>
+              Application that can be installed on the users computer that
+              allows them to write notes or code snippets in the browser. App
+              functions offline as well.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small>
+            <Button variant="success"><a href="https://jate-pwa-editor.herokuapp.com/"></a>Deployment</Button>{' '}
+              <Button variant="secondary"><a href="https://github.com/jdellis490/TextEditor"></a>GitHub</Button>
+            </small>
+          </Card.Footer>
+        </Card>
+        <Card className="">
+          <Card.Img variant="top" src={socialAPI} />
+          <Card.Body>
+            <Card.Title>Social Network API</Card.Title>
+            <Card.Text>
+              I built this project to create tool for users to generate complex
+              and secure passwords that they can use for various accounts.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Backend Application</small>
+            <br></br>
+            <small>
+            <Button variant="success"><a href="https://github.com/jdellis490/Social-Network-API">
+                
+              </a>GitHub</Button>
+            </small>
+          </Card.Footer>
+        </Card>
+      </div>
+  );
+};
 
-Quisque auctor quam velit, quis luctus arcu sodales ac. In aliquet nunc rutrum erat convallis, a rutrum ipsum bibendum. Suspendisse potenti. Sed et maximus odio. Aliquam erat volutpat. Vestibulum semper, leo ut lacinia elementum, augue tellus efficitur nibh, non molestie erat est sollicitudin ligula. Vestibulum mauris quam, faucibus vel venenatis eu, posuere accumsan neque.
-
-Aliquam commodo augue a pharetra feugiat. Donec id eros augue. Duis viverra dolor eget ante maximus molestie. Sed id odio in nunc congue dapibus vel non dolor. Aliquam a interdum ipsum. Cras tristique sem nec tempor consequat. Ut venenatis tristique dui. Aenean viverra, elit sit amet bibendum bibendum, risus nibh viverra orci, et ultrices sapien tortor id est. Morbi malesuada condimentum euismod. Aenean hendrerit quam in euismod molestie. In tempor sapien tincidunt mauris dictum consequat. Ut luctus placerat orci, non dictum urna gravida sed. Ut aliquam sem sit amet enim ullamcorper, sed vulputate eros faucibus. Sed porttitor risus hendrerit facilisis posuere. Phasellus ac odio ultricies, sagittis nisl in, luctus tortor.
-
-Sed ac augue non ligula mattis dignissim ut sit amet magna. Aenean ut ipsum eleifend, cursus augue vel, egestas magna. Quisque blandit massa id risus tristique lacinia. Vestibulum gravida interdum augue sed condimentum. Cras vel orci sodales, laoreet ligula nec, sagittis arcu. Vestibulum ac odio sem. Nunc tempor nulla elit, nec tristique ligula imperdiet ac.
-
-Maecenas ut odio ante. Donec bibendum ipsum risus, blandit efficitur lacus commodo sed. Vestibulum elementum ultricies dolor, ut faucibus arcu molestie convallis. Maecenas id nisi in neque bibendum vehicula at ac quam. Integer eu lectus at risus luctus ultricies. Integer dictum malesuada efficitur. Proin lobortis eros quam, in gravida dui cursus ac. Maecenas ligula nisl, ultrices in lobortis at, dignissim nec neque. Proin posuere suscipit tellus. Praesent non risus a justo ornare posuere ut et ipsum. Pellentesque posuere fringilla justo ut pellentesque.</p>
-    </div>
-  )
-}
-
-export default Project
+export default Project;
